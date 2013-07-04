66faf9a6044bafc6cf172169b72315d152480f24
niloufar <manew1n-jewr@yahoo.co.jp>
2013-07-04 19:36:10 +0900

    ビルド スクリプトを２つ追加した。 pkg-config-0.28.sh xz-5.0.5.sh

---
+++ local/src/packaging/xz-5.0.5.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+# vim: tabstop=4 fileformat=unix fileencoding=utf-8 filetype=sh
+
+# license: Apatche v2.0
+
+if [ "" = "${IN_PACKAGE_SCRIPT}" ]
+then
+	echo FAIL: \${XMINGW}/package から実行してください。
+	exit 1
+fi
+
+
+# ARCH は package が設定している。
+# XLIBRARY_SOURCES は xmingw のための環境変数鵜。 env.sh で設定している。
+init_var() {
+	# package に返す変数。
+	MOD=xz
+	if [ "" = "${VER}" ]
+	then
+	VER=5.0.5
+	REV=1
+	fi
+	DIRECTORY="${MOD}-${VER}"
+
+	# 内部で使用する変数。
+	__ARCHIVEDIR="${XLIBRARY_SOURCES}/libs/compress"
+	__ARCHIVE="${MOD}-${VER}"
+
+	__BINZIP=${MOD}-${VER}-${REV}-bin_${ARCHSUFFIX}
+	__DEVZIP=${MOD}-dev-${VER}-${REV}_${ARCHSUFFIX}
+	__TOOLSZIP=${MOD}-${VER}-${REV}-tools_${ARCHSUFFIX}
+}
+
+dependencies() {
+	cat <<EOS
+libiconv
+gettext
+EOS
+}
+
+optional_dependencies() {
+	cat <<EOS
+EOS
+}
+
+license() {
+	cat <<EOS
+public domain
+
+GNU LGPLv2.1+
+
+GNU GPLv2+
+
+EOS
+}
+
+run_expand_archive() {
+local name
+	name=`find_archive "${__ARCHIVEDIR}" ${__ARCHIVE}` &&
+	expand_archive "${__ARCHIVEDIR}/${name}"
+}
+
+run_configure() {
+	CC="gcc `${XMINGW}/cross --archcflags`" \
+	CPPFLAGS="`${XMINGW}/cross --cflags`" \
+	LDFLAGS="`${XMINGW}/cross --ldflags` \
+	-Wl,--enable-auto-image-base -Wl,-s" \
+	CFLAGS="-pipe -O2 -fomit-frame-pointer -ffast-math  -static-libgcc" \
+	${XMINGW}/cross-configure --disable-gtk-doc --disable-static --prefix="${INSTALL_TARGET}"
+}
+
+run_make() {
+	${XMINGW}/cross make all install
+}
+
+run_pack() {
+	cd "${INSTALL_TARGET}" &&
+	pack_archive "${__BINZIP}" bin/*.dll &&
+	pack_archive "${__DEVZIP}" include lib/*.a share/doc &&
+	pack_archive "${__TOOLSZIP}" bin/*.{exe,manifest,local} bin/zx{diff,grep,less,more} share/man/man1 share/locale &&
+	store_packed_archive "${__BINZIP}" &&
+	store_packed_archive "${__DEVZIP}" &&
+	store_packed_archive "${__TOOLSZIP}"
+}
+
+
+
