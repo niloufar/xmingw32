45008edba02dc1c5e062e27783a32572e2467ae6
niloufar <manew1n-jewr@yahoo.co.jp>
2014-02-03 22:39:52 +0900

    ビルド スクリプトを追加した。 JasPer のビルド スクリプト jasper.1.900.1.sh を作成した。

---
+++ local/src/packaging/jasper.1.900.1.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+# vim: tabstop=4 fileformat=unix fileencoding=utf-8 filetype=sh
+
+# license: Apatche v2.0
+
+if [ "" = "${IN_PACKAGE_SCRIPT}" ]
+then
+	echo FAIL: \${XMINGW}/package から実行してください。
+	exit 1
+fi
+
+
+# ARCH は package が設定している。
+# XLIBRARY_SOURCES は xmingw のための環境変数。 env.sh で設定している。
+init_var() {
+	#XLIBRARY_SET=${XLIBRARY}/gimp_build_set
+
+	# package に返す変数。
+	MOD=jasper
+	if [ "" = "${VER}" ]
+	then
+	VER=1.900.1
+	REV=1
+#	PATCH=2.debian
+	fi
+	DIRECTORY="${MOD}-${VER}"
+
+	# 内部で使用する変数。
+	__ARCHIVEDIR="${XLIBRARY_SOURCES}/libs/pic"
+	__ARCHIVE="${MOD}-${VER}"
+#	__PATCH_ARCHIVE="${MOD}_${VER}-${PATCH}"
+
+	__BINZIP=${MOD}-${VER}-${REV}-bin_${ARCHSUFFIX}
+	__DEVZIP=${MOD}-dev-${VER}-${REV}_${ARCHSUFFIX}
+	__TOOLSZIP=${MOD}-${VER}-${REV}-tools_${ARCHSUFFIX}
+}
+
+dependencies() {
+	cat <<EOS
+EOS
+}
+
+optional_dependencies() {
+	cat <<EOS
+EOS
+}
+
+license() {
+	cat <<EOS
+JasPer License Version 2.0
+EOS
+}
+
+run_expand_archive() {
+local name
+	name=`find_archive "${__ARCHIVEDIR}" ${__ARCHIVE}` &&
+	expand_archive "${__ARCHIVEDIR}/${name}"
+}
+
+run_configure() {
+	CC="gcc `${XMINGW}/cross --archcflags`" \
+	CPPFLAGS="`${XMINGW}/cross --cflags`" \
+	LDFLAGS="`${XMINGW}/cross --ldflags` \
+	-ljpeg \
+	-Wl,--enable-auto-image-base -Wl,-s" \
+	CFLAGS="-pipe -O2 -fomit-frame-pointer -ffast-math  -static-libgcc" \
+	${XMINGW}/cross-configure --enable-shared --disable-static --without-x --prefix="${INSTALL_TARGET}"
+}
+
+post_configure() {
+	# shared ファイルを作ってくれない場合の対処。
+	# static なライブラリーのリンクはこうしないと libtool がいろいろ面倒をみてしまう。
+	bash ${XMINGW}/replibtool.sh shared mix
+}
+
+run_make() {
+	${XMINGW}/cross make all install
+}
+
+pre_pack() {
+	cd "${INSTALL_TARGET}" &&
+	mkdir -p share &&
+	mv man share/.
+#	echo skip > /dev/null
+}
+
+run_pack() {
+	cd "${INSTALL_TARGET}" &&
+	pack_archive "${__BINZIP}" bin/*.dll &&
+	pack_archive "${__DEVZIP}" include lib/*.a &&
+	pack_archive "${__TOOLSZIP}" bin/*.{exe,manifest,local} share/man/man1 &&
+	store_packed_archive "${__BINZIP}" &&
+	store_packed_archive "${__DEVZIP}" &&
+	store_packed_archive "${__TOOLSZIP}"
+}
+
+
+
