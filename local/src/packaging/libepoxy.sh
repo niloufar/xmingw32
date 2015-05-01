d22f198a823689d742999352ac6a03897b3d5fe5
niloufar <manew1n-jewr@yahoo.co.jp>
2015-05-01 15:28:07 +0900

    ビルド スクリプトを追加した。

---
+++ local/src/packaging/libepoxy.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+# vim: tabstop=4 fileformat=unix fileencoding=utf-8 filetype=sh
+
+# license: Apatche v2.0
+
+if [ "" = "${IN_PACKAGE_SCRIPT}" ]
+then
+	echo "FAIL: \${XMINGW}/package から実行してください。"
+	exit 1
+fi
+
+
+# ARCH は package が設定している。
+# XLIBRARY_SOURCES は xmingw のための環境変数。 env.sh で設定している。
+init_var() {
+	#XLIBRARY_SET=${XLIBRARY}/gimp_build_set
+
+	# package に返す変数。
+	MOD=libepoxy
+	if [ "" = "${VER}" ]
+	then
+	VER="git-20062c2"
+	REV=1
+#	PATCH=2.debian
+	fi
+	DIRECTORY="${MOD}-${VER}"
+
+	# 内部で使用する変数。
+	__ARCHIVEDIR="${XLIBRARY_SOURCES}/libs/pic"
+	__ARCHIVE="${MOD}-${VER}"
+#	__PATCH_ARCHIVE="${MOD}_${VER}-${PATCH}"
+
+	__BINZIP=${MOD}-${VER}-${REV}-bin_${ARCHSUFFIX}
+	__DEVZIP=${MOD}-dev-${VER}-${REV}_${ARCHSUFFIX}
+}
+
+dependencies() {
+	cat <<EOS
+jpeg
+libpng
+tiff
+zlib
+EOS
+}
+
+optional_dependencies() {
+	cat <<EOS
+EOS
+}
+
+license() {
+	cat <<EOS
+MIT license
+
+EOS
+}
+
+
+run_expand_archive() {
+local name
+	name=`find_archive "${__ARCHIVEDIR}" ${__ARCHIVE}` &&
+	expand_archive "${__ARCHIVEDIR}/${name}"
+}
+
+pre_configure() {
+	if [ ! -e configure ]
+	then
+		NOCONFIGURE=1 $XMINGW/cross-host sh ./autogen.sh
+	fi
+}
+
+run_configure() {
+	CC="gcc `${XMINGW}/cross --archcflags`" \
+	CXX="g++ `${XMINGW}/cross --archcflags`" \
+	CPPFLAGS="`${XMINGW}/cross --cflags`" \
+	LDFLAGS="`${XMINGW}/cross --ldflags` \
+	-Wl,--enable-auto-image-base -Wl,-s" \
+	CFLAGS="-pipe -O2 -fomit-frame-pointer -ffast-math " \
+	CXXFLAGS="-pipe -O2 -fomit-frame-pointer -ffast-math " \
+	${XMINGW}/cross-configure --enable-shared --disable-static --prefix="${INSTALL_TARGET}"
+}
+
+run_make() {
+	${XMINGW}/cross make all install
+}
+
+run_pack() {
+	cd "${INSTALL_TARGET}" &&
+	pack_archive "${__BINZIP}" bin/*.dll &&
+	pack_archive "${__DEVZIP}" include lib/*.a lib/pkgconfig  &&
+	store_packed_archive "${__BINZIP}" &&
+	store_packed_archive "${__DEVZIP}"
+}
+
+
+
