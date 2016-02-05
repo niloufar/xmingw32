3b5b621facfe6ad9823c509857641d71934d64b6
niloufar <manew1n-jewr@yahoo.co.jp>
2016-02-05 23:49:43 +0900

    Harfbuzz を shared でビルドするようにした。

--- local/src/packaging/harfbuzz-0.9.21.sh
+++ local/src/packaging/harfbuzz-0.9.21.sh
@@ -26,7 +26,7 @@ init_var() {
 	__ARCHIVEDIR="${XLIBRARY_SOURCES}/libs/pic"
 	__ARCHIVE="${MOD}-${VER}"
 
-#	__BINZIP=${MOD}-${VER}-${REV}-bin_${ARCHSUFFIX}
+	__BINZIP=${MOD}-${VER}-${REV}-bin_${ARCHSUFFIX}
 	__DEVZIP=${MOD}-dev-${VER}-${REV}_${ARCHSUFFIX}
 	__TOOLSZIP=${MOD}-${VER}-${REV}-tools_${ARCHSUFFIX}
 }
@@ -88,7 +88,7 @@ run_configure_xp() {
 	LDFLAGS="`${XMINGW}/cross --ldflags` \
 	-Wl,--enable-auto-image-base -Wl,-s" \
 	CFLAGS="-pipe -O2 -fomit-frame-pointer -ffast-math" \
-	${XMINGW}/cross-configure --disable-shared --enable-static --prefix="${INSTALL_TARGET}" --with-uniscribe=no
+	${XMINGW}/cross-configure --enable-shared --disable-static --prefix="${INSTALL_TARGET}" --with-uniscribe=no
 }
 
 run_configure() {
@@ -97,7 +97,19 @@ run_configure() {
 	LDFLAGS="`${XMINGW}/cross --ldflags` \
 	-Wl,--enable-auto-image-base -Wl,-s" \
 	CFLAGS="-pipe -O2 -fomit-frame-pointer -ffast-math" \
-	${XMINGW}/cross-configure --disable-shared --enable-static --prefix="${INSTALL_TARGET}" --with-uniscribe=auto
+	${XMINGW}/cross-configure --enable-shared --disable-static --prefix="${INSTALL_TARGET}" --with-uniscribe=auto
+}
+
+post_configure() {
+	# 使用する場合は bash ${XMINGW}/replibtool.sh にオプションを並べる。
+	# shared ファイルを作ってくれない場合の対処。
+#	bash ${XMINGW}/replibtool.sh shared
+	# static なライブラリーのリンクはこうしないと libtool がいろいろ面倒をみてしまう。
+#	bash ${XMINGW}/replibtool.sh mix
+	# libstdc++ を静的リンクする。
+	bash ${XMINGW}/replibtool.sh static-libgcc
+	# 追加で libtool を書き換える場合は replibtool.sh の実行後に行う。
+#	echo skip > /dev/null
 }
 
 run_make() {
@@ -116,10 +128,10 @@ local UNISCRIBE_LIBS="-lusp10 -lgdi32 -lrpcrt4"
 
 run_pack() {
 	cd "${INSTALL_TARGET}" &&
-#	pack_archive "${__BINZIP}" bin/*.dll &&
+	pack_archive "${__BINZIP}" bin/*.dll &&
 	pack_archive "${__DEVZIP}" include lib/*.{def,a} lib/pkgconfig &&
 	pack_archive "${__TOOLSZIP}" bin/*.{exe,manifest,local} &&
-#	store_packed_archive "${__BINZIP}" &&
+	store_packed_archive "${__BINZIP}" &&
 	store_packed_archive "${__DEVZIP}" &&
 	store_packed_archive "${__TOOLSZIP}"
 }
