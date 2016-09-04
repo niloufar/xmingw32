6f5823718513fe981334a23196884734eadba6c1
niloufar <manew1n-jewr@yahoo.co.jp>
2016-09-04 15:50:48 +0900

    ビルド スクリプトを追加した。

---
+++ local/src/packaging/libwebp-0.5.1.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+# vim: tabstop=4 fileformat=unix fileencoding=utf-8 filetype=sh
+
+# license: Apatche v2.0
+
+if [ "" = "${IN_PACKAGE_SCRIPT}" ]
+then
+	echo "FAIL: \${XMINGW}/package から実行してください。"
+	exit 1
+fi
+
+
+# ARCH は package が設定している。
+# XLIBRARY_SOURCES は xmingw のための環境変数。 env.sh で設定している。
+init_var() {
+	XLIBRARY_SET=${XLIBRARY}/gimp_build_set
+
+	# package に返す変数。
+	MOD=libwebp
+	[ "" = "${VER}" ] && VER=0.5.1
+	[ "" = "${REV}" ] && REV=1
+#	[ "" = "${PATCH}" ] && PATCH=2.debian
+	DIRECTORY="${MOD}-${VER}"
+
+	# 内部で使用する変数。
+	__ARCHIVEDIR="${XLIBRARY_SOURCES}/libs/pic"
+	__ARCHIVE="${MOD}-${VER}"
+#	__PATCH_ARCHIVE="${MOD}_${VER}-${PATCH}"
+
+	__BINZIP=${MOD}-${VER}-${REV}-bin_${ARCHSUFFIX}
+	__DEVZIP=${MOD}-dev-${VER}-${REV}_${ARCHSUFFIX}
+	__TOOLSZIP=${MOD}-${VER}-${REV}-tools_${ARCHSUFFIX}
+}
+
+dependencies() {
+	cat <<EOS
+EOS
+}
+
+optional_dependencies() {
+	cat <<EOS
+giflib
+jpeg
+libpng
+opengl
+tiff
+EOS
+}
+
+license() {
+	cat <<EOS
+BSD License
+
+EOS
+}
+
+
+run_expand_archive() {
+local name
+	name=`find_archive "${__ARCHIVEDIR}" ${__ARCHIVE}` &&
+	expand_archive "${__ARCHIVEDIR}/${name}"
+}
+
+run_configure() {
+	CC="gcc `${XMINGW}/cross --archcflags`" \
+	CXX="g++ `${XMINGW}/cross --archcflags`" \
+	CPPFLAGS="`${XMINGW}/cross --cflags`" \
+	LDFLAGS="`${XMINGW}/cross --ldflags` \
+	-Wl,--enable-auto-image-base -Wl,-s" \
+	CFLAGS="-pipe -O2 -fomit-frame-pointer -ffast-math " \
+	CXXFLAGS="-pipe -O2 -fomit-frame-pointer -ffast-math ${OLD_CXX_ABI} " \
+	${XMINGW}/cross-configure --enable-shared --disable-static --enable-png --enable-jpeg --enable-tiff --enable-gif --enable-wic  --enable-sse2 --enable-sse4.1 --enable-avx2  --enable-libwebpmux --enable-libwebpdemux --enable-libwebpdecoder --enable-libwebpextras --prefix="${INSTALL_TARGET}"
+}
+
+run_make() {
+	${XMINGW}/cross make all install
+}
+
+pre_pack() {
+local docdir="${INSTALL_TARGET}/share/doc/${MOD}"
+	mkdir -p "${docdir}" &&
+	cp COPYING PATENTS "${docdir}/."
+}
+
+run_pack() {
+	cd "${INSTALL_TARGET}" &&
+	pack_archive "${__BINZIP}" bin/*.dll share/doc &&
+	pack_archive "${__DEVZIP}" include lib/*.a lib/pkgconfig &&
+	pack_archive "${__TOOLSZIP}" bin/*.{exe,manifest,local} share/man/man1 &&
+	store_packed_archive "${__BINZIP}" &&
+	store_packed_archive "${__DEVZIP}" &&
+	store_packed_archive "${__TOOLSZIP}"
+}
+
+
+
