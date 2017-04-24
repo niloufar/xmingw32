c1848bb52e1ae36397af6c3ea3bd02a48387877f
niloufar <manew1n-jewr@yahoo.co.jp>
2017-04-25 00:04:59 +0900

    ビルド スクリプトを追加した。  Gtk3 でアイコン セットが必要になる。

---
+++ local/src/packaging/adwaita-icon-theme-3.24.0.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+# vim: tabstop=4 fileformat=unix fileencoding=utf-8 filetype=sh
+
+# license: Apache v2.0
+
+if [ "" = "${IN_PACKAGE_SCRIPT}" ]
+then
+	echo "FAIL: \${XMINGW}/package から実行してください。"
+	exit 1
+fi
+
+
+# ARCH は package が設定している。
+# XLIBRARY_SOURCES は xmingw のための環境変数。 env.sh で設定している。
+init_var() {
+	#XLIBRARY_SET=${XLIBRARY}/gimp_build_set
+
+	# package に返す変数。
+	MOD=adwaita-icon-theme
+	[ "" = "${VER}" ] && VER=3.24.0
+	[ "" = "${REV}" ] && REV=1
+#	[ "" = "${PATCH}" ] && PATCH=2.debian
+	DIRECTORY="${MOD}-${VER}"
+
+	# 内部で使用する変数。
+	__ARCHIVEDIR="${XLIBRARY_SOURCES}/gtk+"
+	__ARCHIVE="${MOD}-${VER}"
+#	__PATCH_ARCHIVE="${MOD}_${VER}-${PATCH}"
+
+	__BINZIP=${MOD}-${VER}-${REV}-bin_${ARCHSUFFIX}
+	__DEVZIP=${MOD}-dev-${VER}-${REV}_${ARCHSUFFIX}
+#	__DOCZIP=${MOD}-${VER}-${REV}-doc_${ARCHSUFFIX}
+#	__TOOLSZIP=${MOD}-${VER}-${REV}-tools_${ARCHSUFFIX}
+}
+
+dependencies() {
+	cat <<EOS
+EOS
+}
+
+optional_dependencies() {
+	cat <<EOS
+EOS
+}
+
+license() {
+	cat <<EOS
+GNU LESSER GENERAL PUBLIC LICENSE
+Version 3, 29 June 2007
+Creative Commons — Attribution-ShareAlike 3.0 Unported — CC BY-SA
+Version 3.0
+EOS
+}
+
+
+run_expand_archive() {
+local name
+	name=`find_archive "${__ARCHIVEDIR}" ${__ARCHIVE}` &&
+	expand_archive "${__ARCHIVEDIR}/${name}"
+}
+
+run_patch() {
+local name
+	# name=`find_archive "${__ARCHIVEDIR}" ${__PATCH_ARCHIVE}` &&
+	# 	patch_debian "${__ARCHIVEDIR}/${name}"
+	echo skip > /dev/null
+}
+
+pre_configure() {
+	echo skip > /dev/null
+}
+
+run_configure() {
+	${XMINGW}/cross-configure --prefix="${INSTALL_TARGET}" #--enable-l-xl-variants
+}
+
+run_make() {
+	${XMINGW}/cross make all install
+}
+
+pre_pack() {
+local docdir="${INSTALL_TARGET}/share/doc/${MOD}"
+	mkdir -p "${docdir}" &&
+	# ライセンスなどの情報は share/doc/<MOD>/ に入れる。
+	cp COPYING* AUTHORS "${docdir}/."
+}
+
+run_pack() {
+	# *-bin はランタイムなどアプリに必要なものをまとめる。
+	# dev-* はビルドに必要なものをまとめる。ツールはビルド環境のものを使用し、含めない。
+	# *-tools はその他の実行ファイル等をまとめる。
+	cd "${INSTALL_TARGET}" &&
+	pack_archive "${__BINZIP}" share/{doc,icons} &&
+	store_packed_archive "${__BINZIP}" &&
+	put_exclude_files share/pkgconfig
+}
+
+
+
