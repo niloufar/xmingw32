f3955e753fe86a61686d4f2fe4da1fae28910b89
niloufar <manew1n-jewr@yahoo.co.jp>
2017-12-09 17:38:59 +0900

    scripts/qualitycheck.pkgconfig で sharedlibdir をチェック対象にした。

--- scripts/test/qualitycheck.pkgconfig_test.sh
+++ scripts/test/qualitycheck.pkgconfig_test.sh
@@ -43,18 +43,18 @@ local a
 }
 
 test_check() {
-local path="/usr/xmingw/xmingw32/local/src/build/8165a865c322c507b4e76db65aee213a"
+local addr="/usr/xmingw/xmingw32/local/src/build/8165a865c322c507b4e76db65aee213a"
 local s
 local e
 local a
-local d=`dirname $0`
-	e="WARNING(qualitycheck.pkgconfig): pkgconfig ファイル ./qualitycheck.pkgconfig_test.a.pc に不具合があります。 libdir, includedir, Libs, Cflags に仮インストール先のアドレスが埋め込まれています( fix コマンドで正規化できます)。 exec_prefix が標準ではない形式で記述されています(望ましくない記述です)。 sharedlibdir に仮インストール先のアドレスが埋め込まれています(影響はないと思われます)。"
-	a=`__qualitycheck_pkgconfig_file "${d}/qualitycheck.pkgconfig_test.a.pc" "${path}" 2>&1`
+local d=`dirname "$0"`
+	e="WARNING(qualitycheck.pkgconfig): pkgconfig ファイル ./qualitycheck.pkgconfig_test.a.pc に不具合があります。 libdir, includedir, sharedlibdir, Libs, Libs.private, Cflags に仮インストール先のアドレスが埋め込まれています( fix コマンドで正規化できます)。 exec_prefix が標準ではない形式で記述されています(望ましくない記述です)。"
+	a=`cd "${d}" && __qualitycheck_pkgconfig_file "./qualitycheck.pkgconfig_test.a.pc" "${addr}" 2>&1`
     assertEquals "${e}" "${a}"
 }
 
 test_fix() {
-local path="/usr/xmingw/xmingw32/local/src/build/8165a865c322c507b4e76db65aee213a"
+local addr="/usr/xmingw/xmingw32/local/src/build/8165a865c322c507b4e76db65aee213a"
 local s
 local e
 local a
@@ -63,7 +63,7 @@ local d=`dirname $0`
 exec_prefix=/usr/local
 libdir=${exec_prefix}/lib
 includedir=${prefix}/include/foo
-sharedlibdir=/usr/xmingw/xmingw32/local/src/build/8165a865c322c507b4e76db65aee213a
+sharedlibdir=${prefix}
 
 Name: foo
 URL: http://foo.org
@@ -72,9 +72,9 @@ Version: 17.4.11
 Requires:
 Requires.private: zlib, harfbuzz >= 0.9.19
 Libs: -L${libdir} -lfoo
-Libs.private: -lbz2 -L/usr/xmingw/xmingw32/local/libs64/lib/lib -lpng16
+Libs.private: -lbz2 -L/usr/xmingw/xmingw32/local/libs64/lib/lib -lpng16 -L${libdir}
 Cflags: -I${includedir}'
-	a=`__qualitycheck_pkgconfig_file_fix "${d}/qualitycheck.pkgconfig_test.a.pc" "${path}" 2>&1`
+	a=`__qualitycheck_pkgconfig_file_fix "${d}/qualitycheck.pkgconfig_test.a.pc" "${addr}" 2>&1`
     assertEquals "${e}" "${a}"
 }
 
