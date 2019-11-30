fbe74270b812ee3a1650c5efc0a8c6a16ae08643
niloufar <manew1n-jewr@yahoo.co.jp>
2019-11-30 18:04:45 +0900

    ビルド スクリプトを追加した。 libdatrie-0.2.12.sh, libthai-0.1.28.sh は pango が依存している。

---
+++ local/src/packaging/libdatrie-0.2.12.sh
@@ -0,0 +1,111 @@
+#!/bin/bash
+# vim: tabstop=4 fileformat=unix fileencoding=utf-8 filetype=sh
+
+# license: Apache v2.0
+
+if [ "" = "${IN_PACKAGE_SCRIPT}" ]
+then
+	echo "FAIL: \${XMINGW}/package から実行してください。"
+	exit 1
+fi
+
+
+# ARCHSUFFIX は package が設定している。
+# XLIBRARY_SOURCES は xmingw のための環境変数。 env.sh で設定している。
+init_var() {
+	#XLIBRARY_SET=${XLIBRARY}/gimp_build_set
+
+	# package に返す変数。
+	MOD=libdatrie
+	[ "" = "${VER}" ] && VER=0.2.12
+	[ "" = "${REV}" ] && REV=1
+#	[ "" = "${PATCH}" ] && PATCH=2.debian
+	DIRECTORY="${MOD}-${VER}"
+
+	# 内部で使用する変数。
+	__ARCHIVEDIR="${XLIBRARY_SOURCES}/libs/etc"
+	__ARCHIVE="${MOD}-${VER}"
+#	__PATCH_ARCHIVE="${MOD}_${VER}-${PATCH}"
+
+	__BINZIP="${MOD}-${VER}-${REV}-bin_${ARCHSUFFIX}"
+	__DEVZIP="${MOD}-dev-${VER}-${REV}_${ARCHSUFFIX}"
+	__TOOLSZIP="${MOD}-${VER}-${REV}-tools_${ARCHSUFFIX}"
+
+	# アーキテクチャを指定しない場合は NOARCH=yes する。
+#	NOARCH=yes
+}
+
+dependencies() {
+	cat <<EOS
+EOS
+}
+
+optional_dependencies() {
+	cat <<EOS
+EOS
+}
+
+license() {
+	cat <<EOS
+GNU LESSER GENERAL PUBLIC LICENSE
+Version 2.1, February 1999
+EOS
+}
+
+
+run_expand_archive() {
+local name
+	name=`find_archive "${__ARCHIVEDIR}" ${__ARCHIVE}` &&
+	expand_archive "${__ARCHIVEDIR}/${name}"
+}
+
+run_patch() {
+local name
+	# name=`find_archive "${__ARCHIVEDIR}" ${__PATCH_ARCHIVE}` &&
+	# 	patch_debian "${__ARCHIVEDIR}/${name}"
+	echo skip > /dev/null
+}
+
+run_configure() {
+	# [0.2.12] rpl_malloc とリンクしようとするため
+	#  ac_cv_func_{realloc,malloc}_0_nonnull=yes している。
+	ac_cv_func_realloc_0_nonnull=yes \
+	ac_cv_func_malloc_0_nonnull=yes \
+	CC="gcc `${XMINGW}/cross --archcflags`" \
+	CXX="g++ `${XMINGW}/cross --archcflags`" \
+	CPPFLAGS="`${XMINGW}/cross --cflags`" \
+	LDFLAGS="`${XMINGW}/cross --ldflags` \
+	-Wl,--enable-auto-image-base -Wl,-s" \
+	CFLAGS="-pipe -O2 -fomit-frame-pointer -ffast-math " \
+	CXXFLAGS="-pipe -O2 -fomit-frame-pointer -ffast-math ${OLD_CXX_ABI} " \
+	${XMINGW}/cross-configure --enable-shared --disable-static --prefix="${INSTALL_TARGET}"
+}
+
+run_make() {
+	${XMINGW}/cross make all install
+}
+
+pre_pack() {
+	# ライセンスなどの情報は share/doc/<MOD>/ に入れる。
+	install_license_files "${MOD}" COPYING*
+
+	rm "${INSTALL_TARGET}/bin/trietool-0.2" -f
+	return 0
+}
+
+run_pack() {
+	# *-bin はランタイムなどアプリに必要なものをまとめる。
+	# dev-* はビルドに必要なものをまとめる。ツールはビルド環境のものを使用し、含めない。
+	# *-tools はその他の実行ファイル等をまとめる。
+	cd "${INSTALL_TARGET}" &&
+	pack_archive "${__BINZIP}" bin/*.dll share/doc &&
+	pack_archive "${__DEVZIP}" include lib/*.a lib/pkgconfig &&
+	pack_archive "${__TOOLSZIP}" bin/*.{exe,manifest,local} share/man/man1 &&
+	store_packed_archive "${__BINZIP}" &&
+	store_packed_archive "${__DEVZIP}" &&
+	store_packed_archive "${__TOOLSZIP}" 
+#	put_exclude_files 
+}
+
+
+
