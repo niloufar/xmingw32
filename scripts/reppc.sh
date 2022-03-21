145b54275de67619b4586163bb960481c2c886b8
niloufar <manew1n-jewr@yahoo.co.jp>
2022-03-21 19:07:15 +0900

    reppc.sh の処理を改善した。

--- scripts/reppc.sh
+++ scripts/reppc.sh
@@ -14,19 +14,21 @@ for dir in `find "${XLIBRARY}" -type d -name pkgconfig`
 do
   pre="`dirname "${dir}"`"
   pre="`dirname "${pre}"`"
-  for f in `find "${dir}" -type f -name \*.pc`
+  for pc_file in `find "${dir}" -type f -name \*.pc`
   do
-    case "`sed "${f}" -ne 's/^prefix\s*=\s*//p' 2>&1`" in
+    imp_prefix="$(sed "${pc_file}" -ne '/^prefix\s*=\s*/ {' \
+    	-e 's/^[^=]\+=\s*//p' -e 'q' -e '}' 2>&1)"
+    case "${imp_prefix}" in
     ${pre})
       # ignore
       ;;
     "")
-:      echo "reppc.sh:INFO: ${f} に prefix 行がありません。"
+:      echo "reppc.sh:INFO: ${pc_file} に prefix 行がありません。"
       ;;
     *)
-      echo "reppc.sh:INFO: ${f} の prefix 行を置き換えています。"
-      sed "${f}" -e "s&^prefix\s*=.*&prefix=${pre}&" > "${f}.rep"
-      mv "${f}.rep" "${f}"
+      echo "reppc.sh:INFO: ${pc_file} の prefix 行を置き換えています。"
+      sed "${pc_file}" -e "s&^prefix\s*=.*&prefix=${pre}&" > "${pc_file}.rep"
+      mv "${pc_file}.rep" "${pc_file}"
       ;;
     esac
   done
