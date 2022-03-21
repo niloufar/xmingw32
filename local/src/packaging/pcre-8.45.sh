cbba94e3af9feb9fd3f3e0faf7259747c24b4f1e
niloufar <manew1n-jewr@yahoo.co.jp>
2022-03-21 18:57:44 +0900

    ビルド スクリプトを追加した。

---
+++ local/src/packaging/pcre-8.45.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+# vim: tabstop=4 fileformat=unix fileencoding=utf-8 filetype=sh
+
+# license: Apache v2.0
+
+if [ "" = "${IN_PACKAGE_SCRIPT}" ]
+then
+	echo "FAIL: \${XMINGW}/package から実行してください。"
+	exit 1
+fi
+
+
+# ARCHSUFFIX は package が設定している。
+# XLIBRARY_SOURCES は xmingw のための環境変数。 env.sh で設定している。
+init_var() {
+	#XLIBRARY_SET="set list"
+
+	# package に返す変数。
+	MOD=pcre
+	[ "" = "${VER}" ] && VER=8.45
+	[ "" = "${REV}" ] && REV=1
+#	[ "" = "${PATCH}" ] && PATCH=2.debian
+	DIRECTORY="${MOD}-${VER}"
+
+	# 内部で使用する変数。
+	__ARCHIVEDIR="${XLIBRARY_SOURCES}/libs/text"
+	__ARCHIVE="${MOD}-${VER}"
+#	__PATCH_ARCHIVE="${MOD}_${VER}-${PATCH}"
+
+	__BINZIP="${MOD}-${VER}-${REV}-bin_${ARCHSUFFIX}"
+	__DEVZIP="${MOD}-dev-${VER}-${REV}_${ARCHSUFFIX}"
+	__DOCZIP="${MOD}-${VER}-${REV}-doc_${ARCHSUFFIX}"
+	__TOOLSZIP="${MOD}-${VER}-${REV}-tools_${ARCHSUFFIX}"
+
+	# アーキテクチャを指定しない場合は NOARCH=yes する。
+#	NOARCH=yes
+}
+
+dependencies() {
+	cat <<EOS
+EOS
+}
+
+optional_dependencies() {
+	cat <<EOS
+EOS
+}
+
+license() {
+	cat <<EOS
+BSD LICENCE
+
+EOS
+}
+
+
+run_expand_archive() {
+local name
+	name=`find_archive "${__ARCHIVEDIR}" ${__ARCHIVE}` &&
+	expand_archive "${__ARCHIVEDIR}/${name}"
+}
+
+run_configure() {
+	CC="gcc `${XMINGW}/cross --archcflags`" \
+	CXX="g++ `${XMINGW}/cross --archcflags`" \
+	CPPFLAGS="`${XMINGW}/cross --cflags`" \
+	LDFLAGS="`${XMINGW}/cross --ldflags` \
+	-Wl,--enable-auto-image-base -Wl,-s" \
+	CFLAGS="-pipe -O2 -fomit-frame-pointer -ffast-math " \
+	CXXFLAGS="-pipe -O2 -fomit-frame-pointer -ffast-math ${OLD_CXX_ABI} " \
+	${XMINGW}/cross-configure --enable-shared --disable-static --prefix="${INSTALL_TARGET}" \
+		--enable-pcre16 --enable-pcre32 \
+		--enable-jit \
+		--enable-utf --enable-unicode-properties \
+		--enable-pcregrep-libz --enable-pcregrep-libbz2
+}
+
+run_make() {
+	${XMINGW}/cross make all install
+}
+
+pre_pack() {
+	# ライセンスなどの情報は share/licenses/<MOD>/ に入れる。
+	install_license_files "${MOD}" LICENCE* AUTHORS*
+
+	sed -i "${INSTALL_TARGET}/bin/pcre-config" \
+		-e 's!^prefix=.\+!prefix="`dirname "$0"`/.."!'
+}
+
+run_pack() {
+	# *-bin はランタイムなどアプリに必要なものをまとめる。
+	# dev-* はビルドに必要なものをまとめる。ツールはビルド環境のものを使用し、含めない。
+	# *-tools はその他の実行ファイル等をまとめる。
+	cd "${INSTALL_TARGET}" &&
+	pack_archive "${__BINZIP}" bin/*.dll lib/girepository-* "${LICENSE_DIR}" &&
+	pack_archive "${__DEVZIP}" bin/*-config include lib/*.a lib/pkgconfig &&
+	pack_archive "${__DOCZIP}" share/doc share/man/man3 &&
+	pack_archive "${__TOOLSZIP}" bin/*.{exe,exe.manifest,exe.local} share/man/man1 &&
+	store_packed_archive "${__BINZIP}" &&
+	store_packed_archive "${__DEVZIP}" &&
+	store_packed_archive "${__DOCZIP}" &&
+	store_packed_archive "${__TOOLSZIP}" 
+#	put_exclude_files 
+}
+
+
+
