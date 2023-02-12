19ff0491dda0961dc8c20097e84a1808b454da6e
niloufar <manew1n-jewr@yahoo.co.jp>
2023-02-12 22:57:18 +0900

    ビルド スクリプトを追加した。

---
+++ local/src/packaging/zstd-1.5.2.sh
@@ -0,0 +1,126 @@
+#!/bin/bash
+# vim: tabstop=4 fileformat=unix fileencoding=utf-8 filetype=sh
+
+# license: Apache v2.0
+
+if [ "" = "${IN_PACKAGE_SCRIPT}" ]
+then
+	echo "FAIL: \${XMINGW}/package から実行してください。"
+	exit 1
+fi
+
+
+# ARCHSUFFIX は package が設定している。
+# XLIBRARY_SOURCES は xmingw のための環境変数。 env.sh で設定している。
+init_var() {
+	#XLIBRARY_SET="set list"
+
+	# package に返す変数。
+	MOD=zstd
+	[ "" = "${VER}" ] && VER=1.5.2
+	[ "" = "${REV}" ] && REV=1
+#	[ "" = "${PATCH}" ] && PATCH=2.debian
+	DIRECTORY="${MOD}-${VER}"
+
+	# 内部で使用する変数。
+	__ARCHIVEDIR="${XLIBRARY_SOURCES}/libs/compress"
+	__ARCHIVE="${MOD}-${VER}"
+#	__PATCH_ARCHIVE="${MOD}_${VER}-${PATCH}"
+
+	__BINZIP="${MOD}-${VER}-${REV}-bin_${ARCHSUFFIX}"
+	__DEVZIP="${MOD}-dev-${VER}-${REV}_${ARCHSUFFIX}"
+#	__DOCZIP="${MOD}-${VER}-${REV}-doc_${ARCHSUFFIX}"
+	__TOOLSZIP="${MOD}-${VER}-${REV}-tools_${ARCHSUFFIX}"
+
+	# アーキテクチャを指定しない場合は NOARCH=yes する。
+#	NOARCH=yes
+}
+
+dependencies() {
+	cat <<EOS
+EOS
+}
+
+optional_dependencies() {
+	cat <<EOS
+zlib
+lzma
+lz4
+EOS
+}
+
+license() {
+	cat <<EOS
+BSD License
+
+GNU GENERAL PUBLIC LICENSE
+Version 2, June 1991
+EOS
+}
+
+
+run_expand_archive() {
+local name
+	name=`find_archive "${__ARCHIVEDIR}" ${__ARCHIVE}` &&
+	expand_archive "${__ARCHIVEDIR}/${name}"
+}
+
+# meson を使用する場合。
+run_configure() {
+	cd build/meson &&
+	CFLAGS="`${XMINGW}/cross --archcflags --cflags` \
+		-pipe -O2 -fomit-frame-pointer -ffast-math" \
+	CXXFLAGS="`${XMINGW}/cross --archcflags --cflags` \
+		-pipe -O2 -fomit-frame-pointer -ffast-math ${OLD_CXX_ABI}" \
+	LDFLAGS="`${XMINGW}/cross --ldflags` \
+		-Wl,--enable-auto-image-base -Wl,-s" \
+	${XMINGW}/cross-meson _build --prefix="${INSTALL_TARGET}" --buildtype=release --optimization=2 --default-library=shared \
+		-Dlz4=disabled &&
+	cd ../..
+}
+
+post_configure() {
+	# 使用する場合は bash ${XMINGW}/replibtool.sh にオプションを並べる。
+	# shared ファイルを作ってくれない場合の対処。
+#	bash ${XMINGW}/replibtool.sh shared
+	# __stdcall な関数を適切にエクスポートできない場合の対処。
+#	bash ${XMINGW}/replibtool.sh stdcall
+	# static なライブラリーのリンクはこうしないと libtool がいろいろ面倒をみてしまう。
+#	bash ${XMINGW}/replibtool.sh mix
+	# libstdc++ を静的リンクする。
+#	bash ${XMINGW}/replibtool.sh static-libgcc
+	# 追加で libtool を書き換える場合は replibtool.sh の実行後に行う。
+	echo skip > /dev/null
+}
+
+# meson を使用する場合。
+run_make() {
+	cd build/meson &&
+	WINEPATH="${PWD}/_build/${MOD}" \
+	${XMINGW}/cross ninja -C _build &&
+	WINEPATH="${PWD}/_build/${MOD}" \
+	${XMINGW}/cross ninja -C _build install &&
+	cd ../..
+}
+
+pre_pack() {
+	# ライセンスなどの情報は share/licenses/<MOD>/ に入れる。
+	install_license_files "${MOD}" COPYING* LICENSE*
+}
+
+run_pack() {
+	# *-bin はランタイムなどアプリに必要なものをまとめる。
+	# dev-* はビルドに必要なものをまとめる。ツールはビルド環境のものを使用し、含めない。
+	# *-tools はその他の実行ファイル等をまとめる。
+	cd "${INSTALL_TARGET}" &&
+	pack_archive "${__BINZIP}" bin/*.dll "${LICENSE_DIR}" &&
+	pack_archive "${__DEVZIP}" include lib/*.a lib/pkgconfig &&
+	pack_archive "${__TOOLSZIP}" bin/*.{exe,exe.manifest,exe.local} bin/zstd{grep,less} share/man/man1 &&
+	store_packed_archive "${__BINZIP}" &&
+	store_packed_archive "${__DEVZIP}" &&
+	store_packed_archive "${__TOOLSZIP}" 
+#	put_exclude_files 
+}
+
+
+
